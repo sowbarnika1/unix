8c63eba6816ba8aba53565cab8e1962d5cd121ec
sowbarnika1 <46050816+sowbarnika1@users.noreply.github.com>
2020-08-10 19:43:27 +0530

    Create arithmetic.sh
    Arithmetic operation with and without using expr

---
+++ arithmetic.sh
@@ -0,0 +1,14 @@
+num1=10
+num2=2
+
+echo `expr $num1 + $num2 `
+echo `expr $num1 - $num2 `
+echo `expr $num1 \* $num2 `
+echo `expr $num1 / $num2 `
+echo `expr $num1 % $num2 `
+
+ echo $[ num1 + num2 ]
+ echo $[ num1 - num2 ]
+ echo $[ num1 * num2 ]
+ echo $[ num1 / num2 ]
+ echo $[ num1 % num2 ]
